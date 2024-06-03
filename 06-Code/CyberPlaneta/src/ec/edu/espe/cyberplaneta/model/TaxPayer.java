6fba33717ea84176b1f7096542d1e452047bd89a
AndresIsaiasCedenoCuenca <aicedeno1@espe.edu.ec>
2024-06-03 18:09:47 -0500

    .git

---
+++ 06-Code/CyberPlaneta/src/ec/edu/espe/cyberplaneta/model/TaxPayer.java
@@ -0,0 +1,14 @@
+
+package ec.edu.espe.cyberplaneta.model;
+
+/**
+ *
+ * @author Code Creators,DCCO-ESPE
+ */
+public class TaxPayer {
+    private int id;
+    private String email;
+    private String name;
+    private String password;
+    private boolean accountingDocumentation;
+}

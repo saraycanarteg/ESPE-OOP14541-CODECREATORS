c1e9dffaf5a5562da54484c0c2d6d4beeb8c57f6
Christian Mateo Bonifaz Vásquez <mateobvc@hotmail.com>
2024-06-03 18:29:20 -0500

    CyberPlaneta src version 0.0.4

---
+++ 06-Code/CyberPlaneta/src/ec/edu/espe/cyberplaneta/model/Calendar.java
@@ -0,0 +1,58 @@
+
+package ec.edu.espe.cyberplaneta.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author  Code Creators, DCCO-ESPE
+ */
+public class Calendar {
+    private Date deliveryDate;
+    private Date startDate;
+
+    public Calendar(Date deliveryDate, Date startDate) {
+        this.deliveryDate = deliveryDate;
+        this.startDate = startDate;
+    }
+    
+     public static void SendNotification(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Calendar{" + "deliveryDate=" + deliveryDate + ", startDate=" + startDate + '}';
+    }
+    
+
+    /**
+     * @return the deliveryDate
+     */
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    /**
+     * @param deliveryDate the deliveryDate to set
+     */
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    /**
+     * @return the startDate
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate the startDate to set
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    
+    
+}

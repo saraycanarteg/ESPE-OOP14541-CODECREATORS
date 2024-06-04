d36c72716190e7cc410cc5b30a4f97444250aaaf
AndresIsaiasCedenoCuenca <aicedeno1@espe.edu.ec>
2024-06-03 20:12:54 -0500

    CyberPlaneta src version 0.0.6

---
+++ 06-Code/CyberPlanetaSystem/src/ec/edu/espe/cyberplaneta/model/FileSaver.java
@@ -0,0 +1,35 @@
+
+package ec.edu.espe.cyberplaneta.model;
+
+/**
+ *
+ * @author Code Creators,DCCO-ESPE
+ */
+public class FileSaver {
+    private int numberOfFile;
+    
+    public FileSaver(int numberOfFile){
+        this.numberOfFile = numberOfFile;
+    }
+
+    @Override
+    public String toString() {
+        return "FileSaver{" + "numberOfFile=" + getNumberOfFile() + '}';
+    }
+
+    /**
+     * @return the numberOfFile
+     */
+    public int getNumberOfFile() {
+        return numberOfFile;
+    }
+
+    /**
+     * @param numberOfFile the numberOfFile to set
+     */
+    public void setNumberOfFile(int numberOfFile) {
+        this.numberOfFile = numberOfFile;
+    }
+    
+}
+

ab9a3c8a89b6b0d822e26678dbe0da4bbcb10fc1
saraycanarteg <sacanarte@espe.edu.ec>
2024-06-04 08:12:39 -0500

    CyberPlaneta src v0.0.7

--- 06-Code/CyberPlanetaSystem/src/ec/edu/espe/cyberplaneta/controller/SystemAdministrator.java
+++ 06-Code/CyberPlanetaSystem/src/ec/edu/espe/cyberplaneta/controller/SystemAdministrator.java
@@ -13,24 +13,29 @@ import java.util.Scanner;
  */
 public class SystemAdministrator {
 
-    public static void main(String[] args) {
+       
+     public static void AddNewTaxPayer() {
+              
         String idTaxPayer="";
+        String addAnotherTaxPayer;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Cyber Planeta System V0.0.05");
+        
+        do {
 
         do{
         System.out.print("\nEnter the taxpayer Id: ");
-        idTaxPayer = scanner.next();
+        idTaxPayer = scanner.nextLine();
         }while(idTaxPayer.length()!=12);
         
         System.out.print("Enter the taxpayer's e-mail: ");
-        String emailTaxPayer = scanner.next();
+        String emailTaxPayer = scanner.nextLine();
 
         System.out.print("Enter taxpayer name: ");
-        String nameTaxPayer = scanner.next();
+        String nameTaxPayer = scanner.nextLine();
 
         System.out.print("Enter taxpayer password: ");
-        String passwordTaxPayer = scanner.next();
+        String passwordTaxPayer = scanner.nextLine();
 
         System.out.print("The taxpayer provided documentation? [true/false]: ");
         boolean accountingDocumentation = scanner.nextBoolean();
@@ -38,8 +43,14 @@ public class SystemAdministrator {
         TaxPayer taxPayer = new TaxPayer(idTaxPayer, emailTaxPayer, nameTaxPayer, passwordTaxPayer, accountingDocumentation);
 
         RegisterTaxPayer(taxPayer.toString());
-    }
+        System.out.print("Do you want to add another Tax Payer? (y/n): ");
+        addAnotherTaxPayer = scanner.next();
+        
+        
+         } while (addAnotherTaxPayer.equalsIgnoreCase("y"));
+     }
 
+    
     public static void RegisterTaxPayer(String taxpayer) {
 
         String fileName = "TaxPayerData.csv";
---
+++ 06-Code/CyberPlanetaSystem/src/ec/edu/espe/cyberplaneta/view/CyberPlanetaSystem.java
@@ -0,0 +1,37 @@
+
+package ec.edu.espe.cyberplaneta.view;
+
+import java.util.Scanner;
+import ec.edu.espe.cyberplaneta.controller.SystemAdministrator;
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ *
+ * @author Code Creators, DCCO-ESPE
+ */
+public class CyberPlanetaSystem {
+    
+    public static void main(String[] args) throws IOException, ParseException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cyber Planeta System V 0.0.5");
+        
+        
+        int menuOption;
+        System.out.println("MENU DE OPCIONES");
+        System.out.println("1. Anadir nuevo contribuyente");
+        System.out.println("2. Calendario y agenda de clasificacion");
+        System.out.println("3. Sistema de precios");
+        System.out.println("Option:  ");
+        menuOption = scanner.nextInt();
+        
+        
+        if (menuOption == 1){
+          SystemAdministrator.AddNewTaxPayer();
+        }else{
+            System.out.println(" ");
+        }
+    }
+        
+
+}

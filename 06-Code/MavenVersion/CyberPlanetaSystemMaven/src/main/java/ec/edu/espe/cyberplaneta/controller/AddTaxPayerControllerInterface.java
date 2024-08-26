94640b767c525db5db1c842de8c385283fedab4b
AndresIsaiasCedenoCuenca <aicedeno1@espe.edu.ec>
2024-08-26 03:14:38 -0500

    clean and solid AddTaxPayer

---
+++ 06-Code/MavenVersion/CyberPlanetaSystemMaven/src/main/java/ec/edu/espe/cyberplaneta/controller/AddTaxPayerControllerInterface.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ec.edu.espe.cyberplaneta.controller;
+
+/**
+ *
+ * @author Andres Cedeno,Code Creators,DCCO-ESPE
+ */
+public interface AddTaxPayerControllerInterface {
+    void saveTaxPayer(String id, String email, String name, String password, boolean documentation, String initDateStr, String finishDateStr, String cellNumber);
+}
